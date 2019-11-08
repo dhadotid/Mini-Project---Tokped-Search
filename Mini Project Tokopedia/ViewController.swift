958e8dcd4be0f59b228ccf52aecb43486f91dafc
yudha <yudhapurbajagad@gmail.com>
2019-11-08 09:31:24 +0700

    Initial Commit

---
+++ Mini Project Tokopedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Project Tokopedia
+//
+//  Created by yudha on 08/11/19.
+//  Copyright © 2019 yudha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
